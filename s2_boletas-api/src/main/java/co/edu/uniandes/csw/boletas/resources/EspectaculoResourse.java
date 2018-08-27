2865c1db7db1e38b2674eafa5c67f585e54f1b9d
Sebastian <>
2018-08-27 09:22:29 -0500

    Agregaron Dtos

--- s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/EspectaculoResourse.java
+++ s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/EspectaculoResourse.java
@@ -7,9 +7,13 @@ package co.edu.uniandes.csw.boletas.resources;
 
 import co.edu.uniandes.csw.boletas.dtos.EspectaculoDTO;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
 
 
 
@@ -18,14 +22,23 @@ import javax.ws.rs.Produces;
  *
  * @author estudiante
  */
-@Path("Espectaculos")
+@Path("espectaculos")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class EspectaculoResourse 
 {
+    
+    @POST
     public EspectaculoDTO createEspectaculo(EspectaculoDTO espectaculo)
     {
-        return null;   
+        return espectaculo;   
+    }
+    
+    @GET
+    @Path("{espectaculoId : \\d+}")
+    public EspectaculoDTO getEspectaculo(@PathParam("espectaculoId") Long especatculoId)
+    {
+        return null;
     }
 }
--- s2_boletas-back/src/main/java/co/edu/uniandes/csw/bibilioteca/entities/EspectaculoEntity.java
+++ s2_boletas-back/src/main/java/co/edu/uniandes/csw/bibilioteca/entities/EspectaculoEntity.java
@@ -6,12 +6,31 @@
 package co.edu.uniandes.csw.bibilioteca.entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
 
 /**
  *
- * @author estudiante
+ * @author Sebastian Rodriguez
  */
+@Entity
 public class EspectaculoEntity extends BaseEntity implements Serializable
 {
+    private String nombre;
     
+    private Date fecha;
+    
+    private String descripcion;
+    
+    private String artista;
+    
+    public void cambiarArtista(String artist)
+    {
+        artista = artist;
+    }
+    
+    public String darArtista()
+    {
+        return artista;
+    }
 }
