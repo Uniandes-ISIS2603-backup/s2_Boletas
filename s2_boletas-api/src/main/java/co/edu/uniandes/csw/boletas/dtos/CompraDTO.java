f33f42b7182173c1255fae0ee0af365c99c811c8
estudiante <estudiante@ISIS2603-355.sis.virtual.uniandes.edu.co>
2018-08-27 10:27:43 -0500

    Clases Compra

--- s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/dtos/CompraDTO.java
+++ s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/dtos/CompraDTO.java
@@ -4,11 +4,75 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.boletas.dtos;
-
+import java.util.Date
 /**
  *
- * @author estudiante
+ * @author Gabriel Hamilton
  */
-public class CompraDTO {
+        
+
+public class CompraDTO implements Serializable {
+    
+    private long id;
+    private int costoTotal;
+    private boolean envio;
+    private  Date fecha;
+    private String direccion;
+    
+    public CompraDTO ()
+    {
+        
+    }
+    
+    public long getId()
+    {
+        return id;
+    }
+    
+    public int getCostoTotal()
+    {
+        return costoTotal;
+    }
+    
+    public boolean getEnvio()
+    {
+        return envio;
+    }
+    
+    public Date getFecha()
+    {
+        return fecha;
+    }
+    
+    public String getDireccion()
+    {
+        return direccion;
+    }
+    
+    public void setId(long idE)
+    {
+        id=idE;
+    }
+    
+    public void setCosto(int costoT)
+    {
+        costoTotal= costoT;
+    }
+    
+    public void setEnvio(boolean envioP)
+    {
+        envio= envioP;
+    }
+     
+    public void setDireccion(String direccionE)
+    {
+        direccion= direccionE;
+    }
+    
+    public void setFecha(Date fechaE)
+    {
+        fecha= fechaE;
+    }
+   
     
 }
