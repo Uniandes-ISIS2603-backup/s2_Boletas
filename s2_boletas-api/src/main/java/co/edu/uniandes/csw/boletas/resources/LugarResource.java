717332ae106e25c7306410dab72478d719bbd745
estudiante <estudiante@ISIS2603-373.sis.virtual.uniandes.edu.co>
2018-08-30 00:37:40 -0500

    Implementación resources lugar y silla.

--- s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/LugarResource.java
+++ s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/LugarResource.java
@@ -5,10 +5,18 @@
  */
 package co.edu.uniandes.csw.boletas.resources;
 
+import co.edu.uniandes.csw.bibilioteca.entities.LugarEntity;
+import co.edu.uniandes.csw.boletas.dtos.LugarDTO;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -22,4 +30,38 @@ import javax.ws.rs.Produces;
 public class LugarResource {
     private static final Logger LOGGER = Logger.getLogger(LugarResource.class.getName());
     
+    @POST
+    public LugarDTO createLugar(LugarDTO lugarDTO)
+    {
+        LOGGER.info("LugarResource createLugar: input:" + lugarDTO.toString());
+        LugarEntity lugarEntity = lugarDTO.toEntity();
+        return lugarDTO;
+    }
+    
+    @PUT
+    @Path("(lugar_id : \\d+)")
+    public LugarDTO updateLugar(@PathParam("lugar_id") Long id, LugarDTO lugarDTO)
+    {
+        return lugarDTO;
+    }
+    
+    @GET
+    @Path("(lugar_id : \\d+)")
+    public LugarDTO getLugar(@PathParam("lugar_id") Long id)
+    {
+        return null;
+    }
+    
+    @GET
+    public List<LugarDTO> getLugares()
+    {
+        return null;
+    }
+    
+    @DELETE
+    @Path("(lugar_id : \\d+)")
+    public LugarDTO deleteLugar(@PathParam("lugar_id") Long id)
+    {
+        return null;
+    }
 }
--- s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/SillaResource.java
+++ s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/SillaResource.java
@@ -5,10 +5,18 @@
  */
 package co.edu.uniandes.csw.boletas.resources;
 
+import co.edu.uniandes.csw.bibilioteca.entities.SillaEntity;
+import co.edu.uniandes.csw.boletas.dtos.SillaDTO;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -22,4 +30,40 @@ import javax.ws.rs.Produces;
 @RequestScoped
 public class SillaResource {
     private static final Logger LOGGER = Logger.getLogger(SillaResource.class.getName());
+    
+    @POST
+    public SillaDTO createSilla(SillaDTO sillaDTO)
+    {
+        LOGGER.info("SillaResource createSilla: input: " + SillaResource.class.getName());
+        SillaEntity sillaEntity = sillaDTO.toEntity();
+        return sillaDTO;
+    }
+    
+    @PUT
+    @Path("(silla_id : \\d+)")
+    public SillaDTO updateSilla(@PathParam("silla_id") Long id, SillaDTO sillaDTO)
+    {
+        
+        return sillaDTO;
+    }
+    
+    @GET
+    @Path("(silla_id: \\d+)")
+    public SillaDTO getSilla(@PathParam("silla_id")Long id)
+    {
+        return null;
+    }
+    
+    @GET
+    public List<SillaDTO> getSillas()
+    {
+        return null;
+    }
+    
+    @DELETE
+    @Path("(silla_id: \\d+)")
+    public SillaDTO deleteSilla(@PathParam("silla_id")Long id)
+    {
+        return null;
+    }
 }
