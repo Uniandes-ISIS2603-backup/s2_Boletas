20400a48f7e68bb81a68da38d427d22b26761cf4
jdcamachod <jd.camachod@uniandes.edu.co>
2018-09-18 08:09:45 -0500

    arreglos en Boletalogic

--- s2_boletas-back/src/test/java/co/edu/uniandes/csw/boletas/test/logic/BoletaLogicTest.java
+++ s2_boletas-back/src/test/java/co/edu/uniandes/csw/boletas/test/logic/BoletaLogicTest.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.boletas.test.logic;
 
 import co.edu.uniandes.csw.boletas.ejb.BoletaLogic;
 import co.edu.uniandes.csw.boletas.entities.BoletaEntity;
+import co.edu.uniandes.csw.boletas.entities.EspectaculoEntity;
 import co.edu.uniandes.csw.boletas.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.boletas.persistence.BoletaPersistence;
 import java.util.ArrayList;
@@ -27,8 +28,9 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
- *
- * @author estudiante
+ * Pruebas de logica de boleta
+ * 
+ * @author Diego Camacho
  */
 @RunWith(Arquillian.class)
 public class BoletaLogicTest {
@@ -46,7 +48,13 @@ public class BoletaLogicTest {
     
     private List<BoletaEntity> data = new ArrayList<BoletaEntity>();
     
+
     
+     /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
@@ -57,6 +65,9 @@ public class BoletaLogicTest {
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
     
+    /**
+     *  Configuracion inicial de la prueba
+     */
     @Before
     public void configTest() {
         try {
@@ -95,6 +106,10 @@ public class BoletaLogicTest {
         }
     }
     
+    /**
+     * Prueba para crear una boleta
+     * @throws BusinessLogicException 
+     */
     @Test
     public void createBoletaTest() throws BusinessLogicException {
         BoletaEntity newEntity = factory.manufacturePojo(BoletaEntity.class);
@@ -102,9 +117,41 @@ public class BoletaLogicTest {
         Assert.assertNotNull(result);
         BoletaEntity entity = em.find(BoletaEntity.class, result.getId());
         Assert.assertEquals(newEntity.getId(), entity.getId());
+        Assert.assertEquals(newEntity.getPrecio(), entity.getPrecio());
+        //Assert.assertEquals(newEntity.getFecha(), entity.getFecha());
+        Assert.assertEquals(newEntity.getVendida(), entity.getVendida());
         
     }
     
+    /**
+     * Prueba para crear una boleta con mal espectaculo
+     * @throws BusinessLogicException
+     */
+    @Test(expected = BusinessLogicException.class)
+    public void createBoletaConEspectaculoInvalidoTest() throws BusinessLogicException
+    {
+        BoletaEntity newEntity = factory.manufacturePojo(BoletaEntity.class);
+        newEntity.setEspectaculo(null);
+        boletaLogic.createBoleta(newEntity);
+    }
+    
+    /**
+     * Prueba para crear una boleta con un espectaculo que no existe
+     * @throws BusinessLogicException
+     */
+    public void createBoletaConEspectaculoInvalido2Test() throws BusinessLogicException
+    {
+        BoletaEntity entity = factory.manufacturePojo(BoletaEntity.class);
+        EspectaculoEntity espectaculo = new EspectaculoEntity();
+        espectaculo.setId(Long.MIN_VALUE);
+        entity.setEspectaculo(espectaculo);
+        boletaLogic.createBoleta(entity);
+        
+    }
+    /**
+     * Prueba para borrar una boleta
+     * @throws BusinessLogicException 
+     */
     @Test
     public void deleteBoletaTest() throws BusinessLogicException {
         BoletaEntity entity = data.get(1);
