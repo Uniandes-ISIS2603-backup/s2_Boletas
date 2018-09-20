56759519177f8ac75d1e9512da101830919bf2aa
vm.tirado <vm.tirado@uniandes.edu.co>
2018-09-19 19:34:27 -0500

    cliente  Verficado

---
+++ s2_boletas-back/src/test/java/co/edu/uniandes/csw/boletas/test/logic/ClienteLogicTest.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.boletas.test.logic;
+
+import co.edu.uniandes.csw.boletas.ejb.ClienteLogic;
+import co.edu.uniandes.csw.boletas.entities.ClienteEntity;
+import co.edu.uniandes.csw.boletas.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.boletas.persistence.ClientePersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author estudiante
+ */
+@RunWith(Arquillian.class)
+public class ClienteLogicTest {
+    
+        private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private ClienteLogic clienteLogic;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    private UserTransaction utx;
+            
+    private List<ClienteEntity> data = new ArrayList<ClienteEntity>();
+   
+    
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ClienteEntity.class.getPackage())
+                .addPackage(ClienteLogic.class.getPackage())
+                .addPackage(ClientePersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            System.out.println("EMPIEZO");
+            clearData();
+            System.out.println("CLEAR");
+            insertData();
+            System.out.println("INSERT");
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() {
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            ClienteEntity cliente = factory.manufacturePojo(ClienteEntity.class);
+            em.persist(cliente);
+            data.add(cliente);
+        }
+
+  
+    }
+    
+    @Test
+    public void createClienteTest() throws BusinessLogicException
+    {
+        ClienteEntity newEntity = factory.manufacturePojo(ClienteEntity.class);
+        ClienteEntity result = clienteLogic.createCliente(newEntity);
+        Assert.assertNotNull(result);
+        ClienteEntity entity = em.find(ClienteEntity.class, result.getId());
+        Assert.assertEquals(newEntity.getId(), entity.getId());
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+    
+    @Test
+    public void createClienteNombreTest() throws BusinessLogicException {
+        ClienteEntity newEntity = factory.manufacturePojo(ClienteEntity.class);
+        newEntity.setNombre(data.get(0).getNombre());
+        clienteLogic.createCliente(newEntity);
+    }
+   
+    
+    @Test
+    public void getClienteTest() {
+        System.out.println(""+ data.size());
+        ClienteEntity entity = data.get(0);
+        ClienteEntity resultEntity = clienteLogic.getCliente(entity.getId());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+        Assert.assertEquals(entity.getNombre(), resultEntity.getNombre());
+    }
+    
+     @Test
+    public void updateClienteTest() {
+        ClienteEntity entity = data.get(0);
+        ClienteEntity pojoEntity = factory.manufacturePojo(ClienteEntity.class);
+        pojoEntity.setId(entity.getId());
+        clienteLogic.update( pojoEntity);
+        ClienteEntity resp = em.find(ClienteEntity.class, entity.getId());
+        Assert.assertEquals(pojoEntity.getId(), resp.getId());
+        Assert.assertEquals(pojoEntity.getNombre(), resp.getNombre());
+    }
+    
+    @Test
+    public void deleteClienteTest() throws BusinessLogicException {
+        ClienteEntity entity = data.get(1);
+        clienteLogic.delete(entity.getId());
+        ClienteEntity deleted = em.find(ClienteEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+}
