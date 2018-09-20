db1c59a60015ee22ad874273cb74a3ce2f8c61cf
srodriguezb1 <s.rodriguezb1@uniandes.edu.co>
2018-09-19 22:49:30 -0500

    Se modifico el subrecurso de EspectaculoComentarioLogic

--- s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/EspectaculoComentarioResourse.java
+++ s2_boletas-api/src/main/java/co/edu/uniandes/csw/boletas/resources/EspectaculoComentarioResourse.java
@@ -18,6 +18,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -61,6 +62,14 @@ public class EspectaculoComentarioResourse
         return new ComentarioDTO();
     }
     
+    
+    /**
+     * Metodo para obtener los comentarios asociados a un espectaculo
+     * Tiene anotacion GET y se el pasa como parametro el id del espectaculo
+     * define el recurso de /espectaculos/{id}/comentarios
+     * @param espectaculosId
+     * @return Lista con comentariosDTO
+     */
     @GET
     public List<ComentarioDTO> getComentarios(@PathParam("espectaculosId") Long espectaculosId) {
         LOGGER.log(Level.INFO, "EspectaculoComentarioResourse getComentarios: input: {0}", espectaculosId);
@@ -70,11 +79,19 @@ public class EspectaculoComentarioResourse
         return listaDetailDTOs;
     }
     
+    /**
+     * Metodo GET para devolver un comentario especifico sobre un espectaculo
+     * define el recurso /espectaculos/{id}/comentarios/{id}
+     * @param espectaculosId El espectaculo que tiene ese comentario
+     * @param comentariosId El id del comentario que se esta buscando
+     * @return Un objeto comentarioDTO 
+     * @throws BusinessLogicException En caso de que no exista el comentario o 
+     * no este asociado a ese espectaculo
+     */
     @GET
     @Path("{comentariosId: \\d+}")
     public ComentarioDTO getComentario(@PathParam("espectaculosId") Long espectaculosId, @PathParam("comentariosId") Long comentariosId) throws BusinessLogicException
     {
-        
         
         LOGGER.log(Level.INFO, "EspectaculoComentarioResourse getComentario");
         if(comentarioLogic.getComentario(comentariosId) == null)
@@ -89,10 +106,39 @@ public class EspectaculoComentarioResourse
     }
     
     
+    /**
+     * Metodo que remplaza los comentarios de un espectaculo, por una lista de 
+     * comentarios que le entra como parametro
+     * @param espectaculosId, El id del cual se quiere hacer la actualizacion
+     * @param listaComentarios, Una lista de objetos ComentarioDTO
+     * @return Una lista con los comentarios agregados
+     */
+    @PUT 
+    public List<ComentarioDTO> remplazarComentarios(@PathParam("espectaculosId") Long espectaculosId, List<ComentarioDTO> listaComentarios)
+    {
+        for (ComentarioDTO comentario : listaComentarios) {
+            if (comentarioLogic.getComentario(comentario.getComentarioID()) == null) {
+                throw new WebApplicationException("El recurso /comentarios/" + comentario.getComentarioID() + " no existe.", 404);
+            }
+        }
+    
+         List<ComentarioDTO> listaComentario = comentariosListEntity2DTO(espectaculoComentarioLogic.remplazarComentarios(espectaculosId, booksListDTO2Entity(listaComentarios)));
+    
+        return listaComentario;
+    } 
+    
     private List<ComentarioDTO> comentariosListEntity2DTO(List<ComentarioEntity> entityList) {
         List<ComentarioDTO> list = new ArrayList();
         for (ComentarioEntity entity : entityList) {
             list.add(new ComentarioDTO(entity));
+        }
+        return list;
+    }
+    
+    private List<ComentarioEntity> booksListDTO2Entity(List<ComentarioDTO> dtos) {
+        List<ComentarioEntity> list = new ArrayList<>();
+        for (ComentarioDTO dto : dtos) {
+            list.add(dto.toEntity());
         }
         return list;
     }
