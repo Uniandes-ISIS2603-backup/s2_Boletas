debfd98236a0bec672ee8bf6e2b9a92d616788bd
Jhonatan Amórtegui <ja.amortegui10@uniandes.edu.co>
2018-09-22 01:11:32 -0500

    Implementación de parte faltante de pruebas persistencia.

--- s2_boletas-back/src/test/java/co/edu/uniandes/csw/boletas/test/persistence/LugarPersistenceTest.java
+++ s2_boletas-back/src/test/java/co/edu/uniandes/csw/boletas/test/persistence/LugarPersistenceTest.java
@@ -7,14 +7,18 @@ package co.edu.uniandes.csw.boletas.test.persistence;
 
 import co.edu.uniandes.csw.boletas.entities.LugarEntity;
 import co.edu.uniandes.csw.boletas.persistence.LugarPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -32,6 +36,11 @@ public class LugarPersistenceTest {
     @PersistenceContext
     private EntityManager em;
     
+    @Inject
+    private UserTransaction utx;
+    
+    private List<LugarEntity> data = new ArrayList<LugarEntity>();
+    
     @Deployment
     public static JavaArchive createDeployment()
     {
@@ -40,6 +49,44 @@ public class LugarPersistenceTest {
                 .addPackage(LugarPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml","beans.xml");
+    }
+    
+    @Before
+    public void configTest()
+    {
+        try
+        {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        }catch(Exception e1)
+        {
+            e1.printStackTrace();
+            try
+            {
+                utx.rollback();
+            }catch(Exception e2)
+            {
+                e2.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData()
+    {
+        em.createQuery("delete from LugarEntity").executeUpdate();
+    }
+    private void insertData()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        for(int i = 0; i < 4; i++)
+        {
+            LugarEntity entity = factory.manufacturePojo(LugarEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
     }
     
     @Test
@@ -51,6 +98,29 @@ public class LugarPersistenceTest {
         LugarEntity result = lugarPersistence.create(newEntity);
         
         Assert.assertNotNull(result);
+    }
+    
+
+    
+    @Test
+    public void updateLugarPersistence()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        LugarEntity entity = data.get(0);
+        LugarEntity updater = factory.manufacturePojo(LugarEntity.class);
+        updater.setId(entity.getId());
+        lugarPersistence.update(updater);
+        LugarEntity updated = em.find(LugarEntity.class, entity.getId());
+        Assert.assertEquals(updated, updater);
+    }
+    
+    @Test
+    public void deleteLugarPersistence()
+    {
+        LugarEntity entity = data.get(0);
+        lugarPersistence.delete(entity.getId());
+        LugarEntity deleted = em.find(LugarEntity.class, entity.getId());
+        Assert.assertNull(deleted);
     }
     
 }
